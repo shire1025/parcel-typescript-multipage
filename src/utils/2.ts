fe7701a054dc3c9eef49613b13c319245fcdb5d0
shire <i@janecc.com>
2019-05-27 23:50:07 +0800

    init

---
+++ src/utils/2.ts
@@ -0,0 +1,8 @@
+import "babel-polyfill";
+import msg from "./msg";
+
+const hello = () => {
+  return "Hello World! 22";
+};
+
+console.log(hello());
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import "babel-polyfill";
+import '../assets/custom.scss'
+import msg from "./msg";
+
+const hello = () => {
+  return "Hello World!";
+};
+
+console.log(msg);
